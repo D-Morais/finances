75633767c9276db732833da1eac4e3eda235cf43
D-Morais <moraisdionata@gmail.com>
2024-07-19 01:31:17 -0300

    Adicionando assets e novos arquivos a estrutura do projeto

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from dash import Dash
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__, suppress_callback_exceptions=True)
+estilos = ["https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
+           "https://fonts.googleapis.com/icon?family=Material+Icons", dbc.themes.COSMO]
+dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates@V1.0.4/dbc.min.css"
+
+
+app = Dash(__name__, suppress_callback_exceptions=True, title="MyFinances", external_stylesheets=estilos + [dbc_css])
+app.scripts.config.serve_locally = True
 server = app.server
-app.title = "MyFinances"
